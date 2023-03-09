761fe36ded49663a1c5c3fb0b3ea1843946ae61f
kaseyuina <tomx2nakaji@gmail.com>
2023-03-09 23:13:32 +0900

    Added calculation methods

--- calculator.py
+++ calculator.py
@@ -8,23 +8,24 @@ temp = ""
 print()
 print("Please type in the equation which you would like to calculate.")
 print("(Available operators are: ")
-print("Addition: +, Subtraction: -, Multiplication: *, Division: /")
+print("Addition: +, Subtraction: -, Multiplication: *, Division: /)")
 print("You may also use parentheses.")
 # equation = (input(": "))
+print()
 
 #Unit test case 1
-# equation = "(1+1.25-23)/2*4"
+equation = "(1+1.25-23)/2*4"
 # equation = "(1 + 1.25 - 23) / 2 * 4 "
 # equation = "1 + x + 5"
 
 #Unit test case 2
-equation = "5*4"
+# equation = "5*4"
 # equation = "15/3"
 # equation = ""
 
 
 print()
-print("equation is: " + equation)
+# print("equation is: " + equation)
 
 #Storing equation to the list
 for x, i in enumerate(equation):
@@ -49,8 +50,92 @@ for x, i in enumerate(equation):
     if x + 1 == len(equation):
         equation_list.append(temp)
 
-print(equation_list)
+# print(equation_list)
 
+operator_precedence = {
+    '(' : 0,
+    ')' : 0,
+    '+' : 1,
+    '-' : 1,
+    '*' : 2,
+    '/' : 2
+}
+ 
+def infix_to_postfix(infix):
+    stack = []
+    postfix = [] 
+         
+    for char in infix:
+        if char not in operator_precedence:
+            postfix.append(char)
+        else:
+            if len(stack) == 0:
+                stack.append(char)
+            else:
+                if char == "(":
+                    stack.append(char)
+                elif char == ")":
+                    while stack[len(stack) - 1] != "(":
+                        postfix.append(stack.pop())
+                    stack.pop()
+                elif operator_precedence[char] > operator_precedence[stack[len(stack) - 1]]:
+                    stack.append(char)
+                else:
+                    while len(stack) != 0:
+                        if stack[len(stack) - 1] == '(':
+                            break
+                        postfix.append(stack.pop())
+                    stack.append(char)
+     
+    while len(stack) != 0:
+        postfix.append(stack.pop())
+ 
+    return postfix
+
+
+print(infix_to_postfix(equation_list))
+
+def RPN(states):
+    operator = {
+        '+': (lambda x, y: float(x) + float(y)),
+        '-': (lambda x, y: float(x) - float(y)),
+        '*': (lambda x, y: float(x) * float(y)),
+        '/': (lambda x, y: float(x) / float(y))
+    }
+    stack = []
+    # print('RPN: %s' % states)
+    for index, z  in enumerate(states):
+        # if index > 0:
+            # print(stack)
+        if z not in operator.keys():
+            stack.append(z)
+            continue
+        y = stack.pop()
+        x = stack.pop()
+        stack.append(operator[z](x, y))
+        # print('%s %s %s =' % (x, z, y))
+    # print(stack[0])
+    return stack[0]
+
+# print(RPN(infix_to_postfix(equation_list)))
+
+'''
+def test():
+    print("OK" if RPN("37+621-*+") == 16 else "NG")
+
+if __name__ == '__main__':
+    import sys
+    RPN(sys.argv[1])
+    test()
+'''
+
+
+
+
+
+
+
+'''
 def calc(num1, num2, operator):
     if operator == "+":
         return num1 + num2
@@ -64,6 +149,7 @@ def calc(num1, num2, operator):
             sys.exit()
         else:
             return num1 / num2
+
 
 
 for i in range(len(equation_list)):
@@ -80,3 +166,4 @@ for i in range(len(equation_list)):
 #         print(calc(float(equation_list[i-1]), float(equation_list[i+1]), equation_list[i]))
 
 print(equation_list)
+'''
